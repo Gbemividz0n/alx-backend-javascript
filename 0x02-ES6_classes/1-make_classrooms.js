62874d30b50e1f2f500a44c1eecd5fa998931b7c
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2023-01-05 12:52:26 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,6 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-
-  return [room1, room2, room3];
+  const arr = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return arr;
 }
