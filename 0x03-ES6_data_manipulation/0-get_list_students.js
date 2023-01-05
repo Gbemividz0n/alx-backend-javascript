c6fce9c0cbac32bbd49f63c5701d8c4d6611ccec
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2023-01-05 17:17:40 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,26 +1,10 @@
-export function getListStudents() {
-  const students = [];
+function getListStudents() {
+  return [
+    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    {id: 2, firstName: 'James', location: 'Columbia'},
+    {id: 5, firstName: 'Serena', location: 'San Francisco'}
+  ];
 
-  const student1 = {
-    id: 1,
-    firstName: "Guillaume",
-    location: "San Francisco",
-  };
-  students.push(student1);
-
-  const student2 = {
-    id: 2,
-    firstName: "James",
-    location: "Columbia",
-  };
-  students.push(student2);
-
-  const student3 = {
-    id: 5,
-    firstName: "Serena",
-    location: "San Francisco",
-  };
-  students.push(student3);
-
-  return students;
-}
+const students = getListStudents();
+console.log(students);
+  }
